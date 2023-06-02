fcf69ed00a9a1b2109ca9150161429f1e6df0ea2
Daniel <ttdanny98@gmail.com>
2023-06-01 21:35:18 -0700

    started footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer />
     </div>
   );
 }
