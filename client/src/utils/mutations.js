81549f80631bd90bb8df436cc3359f3ffbab8ab7
Carechiga <Carechiga43@gmail.com>
2023-06-01 20:47:38 -0700

    login and signup pages started with mutations and utils

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,39 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $name: String!
+    $email: String!
+    $password: String!
+    $address: String!
+    $city: String!
+    $zipcode: Number!
+    $phone: Text
+  ) {
+    addUser(
+      name: $name
+      email: $email
+      password: $password
+      address: $address
+      city: $city
+      zipcode: $zipcode
+      phone: $phone
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
--- server/models/User.js
+++ server/models/User.js
@@ -24,7 +24,7 @@ const userSchema = new Schema({
         required: true,
      },
      zipcode: {
-        type: Number,
+        type: Int,
         required: true,
      },
      phone: {
