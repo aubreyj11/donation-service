d091df44a002457ab2a9f941457da7b729f5d1cd
Carechiga <Carechiga43@gmail.com>
2023-06-02 11:50:18 -0700

    react router set up with home. login, and signup endpoints

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,72 @@
+import React from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import Header from './components/Header'
 import Footer from './components/Footer';
+import Home from './pages/Home';
+import NoMatch from './pages/NotFound';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { StoreProvider } from './utils/GlobalState';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-  <>
-      <Header />
-      <Footer />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <StoreProvider>
+            <Header />
+
+          <Routes>              
+            <Route 
+              path="/" 
+              element={<Home />} 
+            />
+            <Route 
+              path="/login" 
+              element={<Login />} 
+            />
+            <Route 
+              path="/signup" 
+              element={<Signup />} 
+            />
+            <Route 
+              path="*" 
+              element={<NoMatch />} 
+            />
+          </Routes>
+            <Footer />
+          </StoreProvider>
+        </div>
+      </Router>    
+    </ApolloProvider>
   );
 }
 
