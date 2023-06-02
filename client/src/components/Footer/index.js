b1d3c47098a80508da7028edb74f4b2d6661b708
Daniel <ttdanny98@gmail.com>
2023-06-02 16:08:17 -0700

    refactored footer.js

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,18 +1,14 @@
-
+import { Segment, Icon, Divider } from 'semantic-ui-react'
 function Footer() {
   return (
-<div class="ui inverted vertical footer segment form-page ">
-    <div class="ui center aligned container">
-
-<div class="ui stackable inverted divided grid">
-    <div class="sixteen wide column center floated column">
-        <a href="https://github.com/aubreyj11/donation-service"><i class=" centered big github icon gth-i"></i></a>
-    </div>
-</div>
-<div class="ui inverted section divider" style={{width: "100%"}}></div>
-    <p style={{fontSize: "15px"}}>©Chari-Team 2023. All Rights Reserved <i class="truck icon"></i></p>
-    </div>
-</div>
+    <Segment basic inverted padded vertical size="small" className='footer-ctn'>
+        <div className='git-ic'>
+            <a href="https://github.com/aubreyj11/donation-service" ><Icon name='github' size="big"/></a>
+            <a href="#top" id='topLink'>Back to Top</a>
+         </div>
+         <Divider inverted section />
+            <p style={{fontSize: "15px", textAlign: "center"}}>©Chari-Team 2023. All Rights Reserved <Icon name='truck' /></p>
+    </Segment>
   )
 }
 
