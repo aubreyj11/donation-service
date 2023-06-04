354054119fc9c6034e45f60d084b745319e5a1c7
Snowman271 <deanbenzmiller@yahoo.com>
2023-06-04 01:34:03 -0700

    setting up resolver backend for stripe

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,35 @@
 const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Order } = require('../models');
+const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
+    Query: {
+
+        order: async (parent, { _id }, context) => {
+          if (context.user) {
+            const user = await User.findById(context.user._id).populate({
+              path: 'orders.donations',
+              populate: 'category'
+            });
+    
+            return user.orders.id(_id);
+          }
+
+          const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items,
+            mode: 'payment',
+            success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `${url}/`
+          });
+    
+          return { session: session.id };
+        }
+      },
     Mutation: {
         addUser
     }
+    
 }
