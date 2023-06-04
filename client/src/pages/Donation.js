282465514e4097e41366dc0c492b28d437b688be
Snowman271 <deanbenzmiller@yahoo.com>
2023-06-04 00:16:19 -0700

    Adding stripe in package.json

--- client/src/pages/Donation.js
+++ client/src/pages/Donation.js
@@ -1,3 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import DonationList from '../components/DonationList';
+
+
+function Signup(props) {
+  return (
+    <>
+    <div className="container my-1">
+      <Link to="/login">← Go to Login</Link>
+    </div>
+    <h2 style={{textAlign: "center"}}>Sign Up</h2>
+    <div style={{ margin: '20px', padding: '0px 20px 0px 20px' }}>
+    <SignupForm/>
+    </div>
+    </>
+  );
+}
+
+export default Donation;
