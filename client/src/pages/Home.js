3385380cfe6b9f93f2ad7b9acced1677abd14f27
Daniel <ttdanny98@gmail.com>
2023-06-05 20:28:48 -0700

    fixed homepage sizing for differents screen sizes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,13 +6,15 @@ function Home() {
       <div className='homePage-seg'>
       <h1 className='home-h1' textAlign="left">Our Mission & Vision</h1>
       <p className='home-text'>Here at CheriTeam, our mission is to empower individuals and communities to make a positive impact in the world through a convenient and transparent donation service program. We aim to bridge the gap between those in need and those who want to help by providing a platform that facilitates meaningful and effective charitable giving. By pursuing these goals, we aim to create a donation service program that empowers individuals to make a lasting and meaningful impact in their local communities.</p>
+    <div className='homePage-img'>
+      <Image style={{ marginTop: "-550px", height: "780px", width: "1100px" }} src='food-Box.jpg' size='big' floated='right' />
+      </div>
       <Button animated color='blue' size='big' type='button' floated='center'>
       <Button.Content visible>Learn More</Button.Content>
       <Button.Content hidden>
         <Icon name='arrow right' />
       </Button.Content>
     </Button>
-      <Image style={{ marginTop: "-550px", height: "980px", width: "1100px" }} src='food-Box.jpg' size='big' floated='right' />
       </div>
     </Segment>
   )
