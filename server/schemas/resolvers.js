af6f3108991c5588b34401c4f0659d481ce76b0c
Snowman271 <deanbenzmiller@yahoo.com>
2023-06-06 18:55:09 -0700

    Added Donation page functionallity & udated stripe

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,6 +6,19 @@ const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
     Query: {
+         User: async (parent, args, context) => {
+        if (context.user) {
+          const user = await User.findById(context.user._id).populate({
+            path: 'donations',
+          });
+  
+          user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+  
+          return user;
+        }
+  
+        throw new AuthenticationError('Not logged in');
+      },
 
         order: async (parent, { _id }, context) => {
           if (context.user) {
@@ -28,8 +41,24 @@ const resolvers = {
           return { session: session.id };
         }
       },
-    Mutation: {
-        addUser
-    }
+      addDonate: async (parent, { Order }, context) => {
+        console.log(context);
+        if (context.user) {
+          const order = new Order({ donation });
+  
+          await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
+  
+          return order;
+        }
+  
+        throw new AuthenticationError('Not logged in');
+      },
+      updateUser: async (parent, args, context) => {
+        if (context.user) {
+          return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+        }
+  
+        throw new AuthenticationError('Not logged in');
+      },
     
 }
