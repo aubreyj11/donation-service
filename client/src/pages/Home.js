102a3aa78ad94aacafa2928fd187c33c81ed1a9a
Travis <traviscmiller01@gmail.com>
2023-06-08 21:21:12 -0700

    refactored home page and profile page button to use semantic ui

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,5 @@
-import { Segment, Image, Button, Icon } from 'semantic-ui-react'
+import { Image, Header, Button, Icon, Grid, Container } from 'semantic-ui-react'
 import { useNavigate } from 'react-router-dom'
-
-
 
 
 
@@ -9,21 +7,31 @@ function Home() {
   const navigate = useNavigate();
 
   return (
-    <Segment basic padded="very">
-      <div className='homePage-seg'>
-      <h1 className='home-h1' textAlign="left">Our Mission & Vision</h1>
-      <p className='home-text'>Here at ChariTeam, our mission is to empower individuals and communities to make a positive impact in the world through a convenient and transparent donation service program. We aim to bridge the gap between those in need and those who want to help by providing a platform that facilitates meaningful and effective charitable giving. By pursuing these goals, we aim to create a donation service program that empowers individuals to make a lasting and meaningful impact in their local communities.</p>
-    <div className='homePage-img'>
-      <Image style={{ marginTop: "-430px", height: "730px", width: "1100px" }} src='food-Box.jpg' size='big' floated='right' />
-      </div>
-      <Button animated color='blue' size='big' type='button' floated='center' onClick={()=>{navigate('/about')}}>
-      <Button.Content visible>Learn More</Button.Content>
-      <Button.Content hidden>
-        <Icon name='arrow right' />
-      </Button.Content>
-      </Button> 
-      </div>
-    </Segment>
+      <Grid columns={2} relaxed='very' stackable padded='horizontally' centered>
+        <Grid.Column width={8}>
+          <Header as={'h2'} style={{fontSize:'36px'}} size='massive' textAlign="center">Our Mission & Vision</Header>
+          <Container style={{fontSize:'24px'}} textAlign='justified' text > 
+            Here at ChariTeam, our mission is to empower individuals and communities to make a positive impact in the world through 
+            a convenient and transparent donation service program. 
+            We aim to bridge the gap between those in need and those who want to help by providing a platform that facilitates meaningful and effective charitable giving. 
+            By pursuing these goals, we aim to create a donation service program that empowers individuals to make a lasting and meaningful impact in their local communities.
+          </Container>
+          <Grid>
+            <Grid.Column style={{marginTop:'20px'}} textAlign='center'>
+          <Button animated color='blue' className='mx-auto' size='medium' type='button' onClick={()=>{navigate('/about')}}>
+          <Button.Content visible>Learn More</Button.Content>
+          <Button.Content hidden>
+            <Icon name='arrow right' />
+          </Button.Content>
+          </Button>
+          </Grid.Column>
+          </Grid>
+        </Grid.Column>
+        <Grid.Column width={5} verticalAlign='middle'>
+          <Image src='food-Box.jpg' fluid />
+        </Grid.Column>
+      </Grid>
+    
 
   )
 }
