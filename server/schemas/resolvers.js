26fc811536b87021b3728355f10b2f8030de347b
Snowman271 <deanbenzmiller@yahoo.com>
2023-06-08 18:48:53 -0700

    fixed resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,40 @@
-const stripe = require('stripe')('stripe key placeholder');
+const stripe = require('stripe')('pk_test_51NGsraCQkZ4sTLVlAxyxwqDcGmDeKmoI6226SLNoBt9Qe9gcYiRUWi4CTIXJ4pqqO8Wp6uITa49l7XFGbvAfTDBz00jxdaatRC');
 const mongoose = require('mongoose');
 const Donation = require('./models');
 const typeDefs = require('./typeDefs');
 
 const resolvers = {
+  Query: {
+    getUser: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id);   
+        return user;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    }
+  },
   Mutation: {
-    createPaymentIntent: async (_, { amount, userId }) => {
+    updateUser: async (parent, {avatar}, context) => {
+      console.log(avatar);
+      try {
+          if (context.user)  {
+              const user = await User.findOneAndUpdate(
+                  { _id: context.user._id }, 
+                  { avatar: avatar }, 
+                  {
+                      new: true,
+                      runValidators: true,
+                  }
+              );
+              return user
+          }
+      } catch (err) {
+          console.log(err);
+      }    
+      throw new AuthenticationError('Not logged in');
+    },
+    createPaymentIntent: async ({ amount, userId }) => {
       try {
         // Create a payment intent
         const paymentIntent = await stripe.paymentIntents.create({
